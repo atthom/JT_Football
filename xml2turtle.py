f6d88cb636a415260055e83c6eaa1b8c75f1e03a
Thomas Jalabert <thom.jalabert@gmail.com>
2018-02-03 19:54:10 +0100

    add script to convert html to turtle data

---
+++ xml2turtle.py
@@ -0,0 +1,30 @@
+import rdflib
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("finalWord.html")
+root = tree.getroot()
+
+for child in root[0]:
+    print(str(child.tag), str(child.attrib))
+
+
+def getCoursToTurtule(intitule, respo_name, respo_surname, respo_email, ects, horaires, semestre, resume):
+    id_cours = intitule.replace(" ", "_")
+    id_respo = respo_surname + "_" + respo_name
+
+    respo = "SI:" + id_respo + " rdf:type owl:NamedIndividual ,\nSI:Professeur ;\n"
+    respo += "SI:Email \"" + respo_email + "\"^^xsd:string ;\n"
+    respo += "SI:Nom \"" + respo_name + "\"^^xsd:string ;\n"
+    respo += "SI:Prenom \"" + respo_surname + "\"^^xsd:string ;\n"
+
+    cours = "SI:" + id_cours + " rdf:type owl:NamedIndividual ,\nSI:Cours ;\n"
+    cours += "SI:Responsable SI:" + id_respo + " ;\n"
+    cours += "SI:ECTS " + ects + " ;\n"
+    cours += "SI:Horaires_Cours " + horaires[0] + " ;\n"
+    cours += "SI:Horaires_Personnel " + horaires[1] + " ;\n"
+    cours += "SI:Horaires_TD " + horaires[2] + " ;\n"
+    cours += "SI:Intitule \"" + intitule + "\"@fr ;\n"
+    cours += "SI:Resume \"" + resume + "\"^^xsd:string ;\n"
+    cours += "SI:Semestre " + semestre + " .\n"
+
+    return respo, cours
