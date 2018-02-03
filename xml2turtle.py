be90e15d79d77963e038f4c8183b64d2daf473cd
Thomas Jalabert <thom.jalabert@gmail.com>
2018-02-03 22:03:57 +0100

    modify html to be parsed. correct minnor bugs in parser

--- xml2turtle.py
+++ xml2turtle.py
@@ -1,4 +1,3 @@
-import rdflib
 import xml.etree.ElementTree as ET
 
 tree = ET.parse("finalWord.html")
@@ -7,6 +6,15 @@ root = tree.getroot()
 for child in root[0]:
     print(str(child.tag), str(child.attrib))
 
+
+intitule = "Distributed Optimization and Games"
+respo_name = "Giovanni"
+respo_surname = "Neglia"
+respo_email = "Giovanni.neglia@sophia.inria.fr"
+ects = 2.00
+horaires = (12, 14, 16)
+semestre = 2
+resume = "The focus of this course is on networks interconnecting decision making elements, whose"
 
 def getCoursToTurtule(intitule, respo_name, respo_surname, respo_email, ects, horaires, semestre, resume):
     id_cours = intitule.replace(" ", "_")
@@ -19,12 +27,19 @@ def getCoursToTurtule(intitule, respo_name, respo_surname, respo_email, ects, ho
 
     cours = "SI:" + id_cours + " rdf:type owl:NamedIndividual ,\nSI:Cours ;\n"
     cours += "SI:Responsable SI:" + id_respo + " ;\n"
-    cours += "SI:ECTS " + ects + " ;\n"
-    cours += "SI:Horaires_Cours " + horaires[0] + " ;\n"
-    cours += "SI:Horaires_Personnel " + horaires[1] + " ;\n"
-    cours += "SI:Horaires_TD " + horaires[2] + " ;\n"
+    cours += "SI:ECTS " + str(ects) + " ;\n"
+    cours += "SI:Horaires_Cours " + str(horaires[0]) + " ;\n"
+    cours += "SI:Horaires_Personnel " + str(horaires[1]) + " ;\n"
+    cours += "SI:Horaires_TD " + str(horaires[2]) + " ;\n"
     cours += "SI:Intitule \"" + intitule + "\"@fr ;\n"
     cours += "SI:Resume \"" + resume + "\"^^xsd:string ;\n"
-    cours += "SI:Semestre " + semestre + " .\n"
+    cours += "SI:Semestre " + str(semestre) + " .\n"
 
     return respo, cours
+
+respo, cours = getCoursToTurtule(intitule, respo_name, respo_surname, respo_email, ects, horaires, semestre, resume)
+
+
+print(respo)
+
+print(cours)
